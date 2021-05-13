5274d3664f78fe1f23c1907cbc8772ee0f70d8b1
Duc Pham <duc@25holdings.com>
2021-05-13 09:42:50 +0700

    -first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require aos
+//= require easing
+//= require jquery.countup
+//= require jquery.waypoints.min
+//= require move-top
+//= require simpleLightbox
+//= require turbolinks
+//= require_tree .
